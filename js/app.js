324904ff11febe38b1d8c6997163d2a063db8872
Jordan Mor <jordanmor@gmail.com>
2017-10-26 16:22:12 -0400

    Improved specialDay function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
-let str = 'There are x days until Halloween!';
+// Old Code
 
-let halloweenDate = new Date(new Date().getFullYear(), 9, 31);
+// let str = 'There are x days until Halloween!';
 
-let complexStr = str.replace('x', dayCount(new Date, halloweenDate));
+// let halloweenDate = new Date(new Date().getFullYear(), 9, 31);
 
-console.log(complexStr);
+// let complexStr = str.replace('x', dayCount(new Date, halloweenDate));
 
-function dayCount(start, end) {
-  return Math.round((end - start)/(1000*60*60*24)); // (ms*sec*min*hr)
+// console.log(complexStr);
+
+// function dayCount(start, end) {
+//   return Math.round((end - start)/(1000*60*60*24)); // (ms*sec*min*hr)
+// }
+
+// New Code
+
+function SpecialDay(name, day, month) {
+	this.name = name;
+	this.day = day;
+	this.month = month;
+	this.date = new Date(new Date().getFullYear(), this.day, this.month);
 }
+
+const halloween = new SpecialDay('Halloween', 9, 31);
+const thanksgiving = new SpecialDay('Thanksgiving', 11, 25);
+const christmas = new SpecialDay('Christmas', 12, 25);
+
+function daysTillHoliday(specialDay) {
+
+	function dayCount(start, end) {
+		return Math.round((end - start)/(1000*60*60*24)); // (ms*sec*min*hr)
+	}
+
+	const daysLeft = dayCount(new Date, specialDay.date);
+
+	return `There are ${daysLeft} days until ${specialDay.name}`;
+}
+console.log(daysTillHoliday(halloween));
+console.log(daysTillHoliday(thanksgiving));
+console.log(daysTillHoliday(christmas));
