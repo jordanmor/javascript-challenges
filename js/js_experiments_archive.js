46897417713ff31966cd4e79bcd82b2378e235bf
Jordan Mor <jordanmor@gmail.com>
2017-11-14 16:47:03 -0500

    Create first basic server using Node.js

--- js/js_experiments_archive.js
+++ js/js_experiments_archive.js
@@ -63,3 +63,16 @@ function printDate() {
 }
 
 console.log(printDate());
+
+
+// ***** MY FIRST SERVER *****
+// Created with Node.js
+
+const http = require('http');
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write("Hello, this is Jordan");
+    response.end();
+}).listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
+});
