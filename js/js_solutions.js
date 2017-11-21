0d7e008ea7041f28e67c4de86f10235611208da1
Jordan Mor <jordanmor@gmail.com>
2017-11-21 13:52:42 -0500

    Solve Record Collection challenge

--- js/js_solutions.js
+++ js/js_solutions.js
@@ -1161,3 +1161,74 @@ function telephoneCheck(str) {
 }
 
 console.log(telephoneCheck("555-555-5555"));
+
+
+// ***** Record Collection *****
+
+/* 
+You are given a JSON object representing a part of your musical album collection. 
+Each album has several properties and a unique id number as its key. Not all albums have complete information.
+
+Write a function which takes an album's id (like 2548), a property prop (like "artist" or "tracks"), 
+and a value (like "Addicted to Love") to modify the data in this collection.
+
+If prop isn't "tracks" and value isn't empty (""), update or set the value for that record album's property.
+Your function must always return the entire collection object.
+There are several rules for handling incomplete data:
+
+If prop is "tracks" but the album doesn't have a "tracks" property, 
+create an empty array before adding the new value to the album's corresponding property.
+
+If prop is "tracks" and value isn't empty (""), push the value onto the end of the album's existing tracks array.
+If value is empty (""), delete the given prop property from the album.
+*/
+
+// Setup
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": []
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value) {
+  let objId = collection[id];
+  if (value === '') {
+    delete objId[prop];
+  } else if (prop !== 'tracks') {
+    objId[prop] = value;
+  } else {
+    // if no tracks prop, add property with value in an new array, otherwise push value into prop array 
+    !objId.hasOwnProperty('tracks') ? objId[prop] = [value] : objId[prop].push(value);
+  }
+ 
+  return collection;
+}
+
+console.log(updateRecords(5439, "artist", "ABBA"));
+// console.log(updateRecords(2548, "artist", ""));
+// console.log(updateRecords(1245, "tracks", "Addicted to Love"));
+// console.log(updateRecords(2468, "tracks", "Free"));
+// console.log(updateRecords(2548, "tracks", ""));
+// console.log(updateRecords(5439, "tracks", "Take a Chance on Me"));
