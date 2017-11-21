937c6353386ff06f6399544d64de8a20e0ebd145
Jordan Mor <jordanmor@gmail.com>
2017-11-21 17:04:18 -0500

    Solve Symmetric Difference challenge

--- js/js_solutions.js
+++ js/js_solutions.js
@@ -1232,3 +1232,32 @@ console.log(updateRecords(5439, "artist", "ABBA"));
 // console.log(updateRecords(2468, "tracks", "Free"));
 // console.log(updateRecords(2548, "tracks", ""));
 // console.log(updateRecords(5439, "tracks", "Take a Chance on Me"));
+
+
+// ***** Symmetric Difference *****
+
+/* 
+Create a function that takes two or more arrays and returns 
+an array of the symmetric difference (△ or ⊕) of the provided arrays.
+
+Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), 
+the mathematical term "symmetric difference" of two sets is the set 
+of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). 
+For every additional symmetric difference you take (say on a set D = {2, 3}), 
+you should get the set with elements which are in either of the two the 
+sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
+*/
+
+function sym(args) {
+  args = [...arguments];
+  
+  return args.reduce((acc, b) => { // reduce two arrays at a time
+    return acc
+      .concat(b) // combine arrays
+      // filter out any number that is found in both arrays
+      .filter( num => !(acc.indexOf(num) !== -1 && b.indexOf(num) !== -1) );
+  // filter out any duplicates found in the final reduced array      
+  }).filter( (num, index, arr) => index === arr.indexOf(num) );
+}
+
+console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]));
