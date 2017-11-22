c17aa6a686360bb6817508dc71ea34b069c4b50d
Jordan Mor <jordanmor@gmail.com>
2017-11-22 17:29:05 -0500

    Solve Make a Person challenge

--- js/js_solutions.js
+++ js/js_solutions.js
@@ -1463,3 +1463,42 @@ var newInv = [
 ];
 
 console.log(updateInventory4(curInv, newInv));
+
+
+// ***** Make a Person *****
+
+/* 
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+
+Run the tests to see the expected output for each method.
+The methods that take an argument must accept only one argument and it has to be a string.
+These methods must be the only available means of interacting with the object.
+*/
+
+var Person = function(firstAndLast) {
+
+  let fullName = firstAndLast;
+
+  this.getFirstName = (  ) => fullName.split(' ')[0];
+  this.getLastName  = (  ) => fullName.split(' ')[1];
+  this.getFullName  = (  ) => fullName;
+  this.setFirstName = name => fullName = `${name} ${fullName.split(' ')[1]}`;
+  this.setLastName  = name => fullName = `${fullName.split(' ')[0]} ${name}`;
+  this.setFullName  = name => fullName = name;
+};
+
+var person = new Person('Bob Ross');
+
+console.log(person.getFullName());
+console.log(person.getFirstName());
+person.setFirstName('Harold');
+console.log(person.getFullName());
+person.setLastName('Curry');
+console.log(person.getFullName());
