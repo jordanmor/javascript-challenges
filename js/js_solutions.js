be7a3c5e1cbe6867e91949de65126397a3dcf943
Jordan Mor <jordanmor@gmail.com>
2017-11-23 00:58:59 -0500

    Solve Map the Debris challenge

--- js/js_solutions.js
+++ js/js_solutions.js
@@ -1534,3 +1534,27 @@ function permAlone(str) {
 }
 
 console.log(permAlone('aab'));
+
+
+// ***** Map the Debris *****
+
+/* 
+Return a new array that transforms the element's average altitude into their orbital periods.
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+You can read about orbital periods on wikipedia.
+
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+*/
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418, earthRadius = 6367.4447;
+  return arr.map(eachObj => {
+    return {
+      name: eachObj.name,
+      orbitalPeriod: Math.round(2 * Math.PI * Math.sqrt(Math.pow((eachObj.avgAlt + earthRadius), 3) / GM ))
+    };
+  });
+}
+
+console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]));
