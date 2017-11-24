4a4e308ff3ec1734e4b5c7cdc5eca4e0d43bdc0f
Jordan Mor <jordanmor@gmail.com>
2017-11-23 19:45:55 -0500

    Solve Pairwise challenge

--- js/js_solutions.js
+++ js/js_solutions.js
@@ -1558,3 +1558,42 @@ function orbitalPeriod(arr) {
 }
 
 console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]));
+
+
+// ***** Pairwise *****
+
+/* 
+Given an array arr, find element pairs whose sum equal the 
+second argument arg and return the sum of their indices.
+If multiple pairs are possible that have the same numeric elements 
+but different indices, return the smallest sum of indices. 
+Once an element has been used, it cannot be reused to pair with another.
+For example pairwise([7, 9, 11, 13, 15], 20) returns 6. 
+The pairs that sum to 20 are [7, 13] and [9, 11]. 
+We can then write out the array with their indices and values.
+
+Index 0 1 2 3 4
+Value 7 9 11  13  15
+Below we'll take their corresponding indices and add them.
+
+7 + 13 = 20 → Indices 0 + 3 = 3
+9 + 11 = 20 → Indices 1 + 2 = 3
+3 + 3 = 6 → Return 6
+*/
+
+function pairwise(arr, arg) {
+   let sum = 0;
+   for (let i = 0; i < arr.length; i++) {
+     for (var j = i + 1; j < arr.length; j++) {
+         if(arr[i] + arr[j] === arg) {
+           sum += i + j;
+           arr[j] = 'a';
+           arr[i] = 'b';
+         }
+       }
+     }
+  
+return sum;
+}
+
+console.log(pairwise([1,4,2,3,0,5], 7));
